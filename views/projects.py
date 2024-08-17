299447e09b9246cd93d66549a0fa00731f517c8e
Mohamed-elaaouam <120938597+Mohamed-elaaouam@users.noreply.github.com>
2024-08-17 18:31:10 +0100

    Update projects.py

--- views/projects.py
+++ views/projects.py
@@ -10,7 +10,7 @@ with col1:
     st.image(["./assets/realproject2.png"],[ st.write(' <a href="https://allopsy.ma/">Allopsy </a>',unsafe_allow_html=True)],width=300,clamp=True)
 
     
-    st.subheader('Side Projects :stuck_out_tongue_closed_eyes: :')
+    st.subheader('Side Projects  I made for fun :stuck_out_tongue_closed_eyes: :')
 
     st.image(["./assets/p1.png"],[  st.write('Cups and Ball made with React you can play it <a href="https://cups-nball.vercel.app/">here </a>',unsafe_allow_html=True)],width=200,clamp=True)
     st.image(["./assets/p2.png"],[ st.write('1bit Space fighter made with Html5 canvas ')],width=200,clamp=True)
