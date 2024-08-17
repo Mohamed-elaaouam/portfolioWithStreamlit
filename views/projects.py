d9bc3fc451c46cdc4457574eaec1120a77ebcfb5
unknown <tribebeholder@gmail.com>
2024-08-17 18:27:17 +0100

    updated projects

--- views/projects.py
+++ views/projects.py
@@ -6,9 +6,14 @@ col1,col2=st.columns(2,gap='medium',vertical_alignment='center')
 
 with col1:
     st.subheader('Made for clients:')
-    st.image(["./assets/p1.png"],[  st.write('Cups and Ball made with React ')],width=200,clamp=True)
-    st.image(["./assets/p2.png"],[ st.write('1bit Space fighter made with Html5 canvas ')],width=200,clamp=True)
+    st.image(["./assets/realproject1.png"],[ st.write('a Platforms serving as phonebook for Moroccan Psychologues check it <a href="https://psychologue.link/">here </a>',unsafe_allow_html=True)],width=300,clamp=True)
+    st.image(["./assets/realproject2.png"],[ st.write(' <a href="https://allopsy.ma/">Allopsy </a>',unsafe_allow_html=True)],width=300,clamp=True)
 
+    
+    st.subheader('Side Projects :stuck_out_tongue_closed_eyes: :')
+
+    st.image(["./assets/p1.png"],[  st.write('Cups and Ball made with React you can play it <a href="https://cups-nball.vercel.app/">here </a>',unsafe_allow_html=True)],width=200,clamp=True)
+    st.image(["./assets/p2.png"],[ st.write('1bit Space fighter made with Html5 canvas ')],width=200,clamp=True)
      
 
 
